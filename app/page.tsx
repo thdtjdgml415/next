7e410f47f03375bb518dfe8ec2f2f5346a704ba4
thdtjdgml415 <thdtjdgml415@naver.com>
2023-12-13 10:33:13 +0900

    gsap 적용

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,13 @@ import backgroundImg from "../public/space.png";
 import backgroundEarth from "../public/earth.png";
 import Image from "next/image";
 import BackGround from "@/components/backGround/BackGround";
+import TransitionLink from "@/components/TransitionLink";
 
 export default async function Home() {
   return (
-    <main className="w-screen h-screen ">
-      <div className="w-screen h-screen overflow-hidden">
-        <BackGround />
-      </div>
-
-      <section></section>
+    <main className="bg-neutral-100 text-black w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
+      <h1 className="text-5xl">HOME PAGE</h1>
+      <TransitionLink href="/about" label="About ->" />
     </main>
   );
 }
